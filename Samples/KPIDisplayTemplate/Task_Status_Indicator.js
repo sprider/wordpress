8a7a1274a8c3c1094caccb5c4802f2fd6a9a7b39
Joseph Velliah <joseph@sprider.org>
2016-12-29 13:44:01 -0600

    implemented pagination

--- Samples/KPIDisplayTemplate/Task_Status_Indicator.js
+++ Samples/KPIDisplayTemplate/Task_Status_Indicator.js
@@ -1,25 +1,37 @@
-var taskstatusViews = taskstatusViews || {};
+window.KPIList = window.KPIList || {};
+window.KPIList.Overrides = {
 
-taskstatusViews.itemStyleOverrides = function (ctx) {
+    customHeader: function (ctx) {        
+        return "<div id='CustomViewHeader' class='container-fluid'><center><p>Report as on today</p></center><table class='table'><thead><tr><th>Title</th><th>Status</th></tr></thead><tbody>";
+    },
+    customItemHtml: function (ctx) {
+        if (ctx.CurrentItem.Status == 'Success') {
+            return '<tr class="active"><td>' + '<a href="' + ctx.CurrentItem.URL + '">' + ctx.CurrentItem.Title + '</a>' + '</td><td>' + '<img src="/sites/dev/SiteAssets/Circle_Green.png" alt="' + ctx.CurrentItem.Status + '"/></td></tr>';
+        }
 
-    if (ctx.CurrentItem.Status == 'Success')
-    {  
-        return '<tr class="active"><td>'+'<a href="' + ctx.CurrentItem.URL + '">' + ctx.CurrentItem.Title + '</a>'+'</td><td>'+ '<img src="/sites/dev/SiteAssets/Circle_Green.png" alt="'+ ctx.CurrentItem.Status +'"/></td></tr>';   
-    }
-    
-    if (ctx.CurrentItem.Status == 'Info')
-    {        
-        return '<tr class="active"><td>'+'<a href="' + ctx.CurrentItem.URL + '">' + ctx.CurrentItem.Title + '</a>'+'</td><td>'+ '<img src="/sites/dev/SiteAssets/Circle_Blue.png" alt="'+ ctx.CurrentItem.Status +'"/></td></tr>';   
-    }
+        if (ctx.CurrentItem.Status == 'Info') {
+            return '<tr class="active"><td>' + '<a href="' + ctx.CurrentItem.URL + '">' + ctx.CurrentItem.Title + '</a>' + '</td><td>' + '<img src="/sites/dev/SiteAssets/Circle_Blue.png" alt="' + ctx.CurrentItem.Status + '"/></td></tr>';
+        }
 
-    if (ctx.CurrentItem.Status == 'Warning')
-    {        
-        return '<tr class="active"><td>'+'<a href="' + ctx.CurrentItem.URL + '">' + ctx.CurrentItem.Title + '</a>'+'</td><td>'+ '<img src="/sites/dev/SiteAssets/Circle_Yellow.png" alt="'+ ctx.CurrentItem.Status +'"/></td></tr>';   
-    }
+        if (ctx.CurrentItem.Status == 'Warning') {
+            return '<tr class="active"><td>' + '<a href="' + ctx.CurrentItem.URL + '">' + ctx.CurrentItem.Title + '</a>' + '</td><td>' + '<img src="/sites/dev/SiteAssets/Circle_Yellow.png" alt="' + ctx.CurrentItem.Status + '"/></td></tr>';
+        }
 
-    if (ctx.CurrentItem.Status == 'Danger')
-    {        
-        return '<tr class="active"><td>'+'<a href="' + ctx.CurrentItem.URL + '">' + ctx.CurrentItem.Title + '</a>'+'</td><td>'+ '<img src="/sites/dev/SiteAssets/Circle_Red.png" alt="'+ ctx.CurrentItem.Status +'"/></td></tr>';   
+        if (ctx.CurrentItem.Status == 'Danger') {
+            return '<tr class="active"><td>' + '<a href="' + ctx.CurrentItem.URL + '">' + ctx.CurrentItem.Title + '</a>' + '</td><td>' + '<img src="/sites/dev/SiteAssets/Circle_Red.png" alt="' + ctx.CurrentItem.Status + '"/></td></tr>';
+        }
+    },
+    pagingControl: function (ctx) {
+        var html = "</tbody></table>";
+        var firstRow = ctx.ListData.FirstRow;
+        var lastRow = ctx.ListData.LastRow;
+        var prev = ctx.ListData.PrevHref;
+        var next = ctx.ListData.NextHref;
+        html += "<nav class='container-fluid'><ul class='pagination'>";
+        html += prev ?"<li class='page-item'><a class='page-link' aria-label='Previous' href='javascript:void(0)' onclick='RefreshPageTo(event, &quot;" + prev + "&quot;); return false;'><span aria-hidden='true'>&laquo;</span><span class='sr-only'>Previous</span></a></li>": "";       
+        html +=  next ? "<li class='page-item'><a class='page-link' aria-label='Next' href='javascript:void(0)' onclick='RefreshPageTo(event, &quot;" + next + "&quot;); return false;'><span aria-hidden='true'>&raquo;</span><span class='sr-only'>Next</span></a></li>" : "";
+        html += "</ul></nav></div>";       
+        return html;
     }
 };
 
@@ -27,21 +39,21 @@ taskstatusViews.itemStyleOverrides = function (ctx) {
     var taskstatusItemCtxOverrides = {};
     taskstatusItemCtxOverrides.Templates = {};
 
-    taskstatusItemCtxOverrides.Templates.Header = "<div id='CustomViewHeader' class='container-fluid'><center><p>Report as on today</p></center><table class='table'><thead><tr><th>Title</th><th>Status</th></tr></thead><tbody>";
-    taskstatusItemCtxOverrides.Templates.Item = taskstatusViews.itemStyleOverrides;
-    taskstatusItemCtxOverrides.Templates.Footer = "</tbody></table></div>";
+    taskstatusItemCtxOverrides.Templates.Header = window.KPIList.Overrides.customHeader;
+    taskstatusItemCtxOverrides.Templates.Item = window.KPIList.Overrides.customItemHtml;
+    taskstatusItemCtxOverrides.Templates.Footer = window.KPIList.Overrides.pagingControl;
 
     taskstatusItemCtxOverrides.ListTemplateType = 100;
     taskstatusItemCtxOverrides.BaseViewID = 1;
-    
-    taskstatusItemCtxOverrides.OnPostRender = function (ctx) {   
-        loadCSS("https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css");     
-        
+
+    taskstatusItemCtxOverrides.OnPostRender = function (ctx) {
+        loadCSS("https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css");
+
         var siteUrl = _spPageContextInfo.siteAbsoluteUrl;
 
         loadScript("https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js", function () {
             loadScript("https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js", function () {
-        });
+            });
         });
     }
 
